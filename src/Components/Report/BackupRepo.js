f616257d25e009b3b6541e91faeae4433367f183
Halima-Ali <halimaali.ha2002@gmail.com>
2023-07-17 15:22:39 +0300

    graph started

--- src/Components/Report/BackupRepo.js
+++ src/Components/Report/BackupRepo.js
@@ -15,7 +15,6 @@ function BackupRepo() {
   const [data, setData] = useState([]);
   const [searchInput, setSearchInput] = useState("");
 
-
   const [portfolio] = state.user?.portfolio_info?.filter(
     (item) => item?.product === "Secure Repositories"
   );
@@ -36,6 +35,16 @@ function BackupRepo() {
       }
     };
 
+    const fetchGraph = async () => {
+      try {
+        const response = await axios.get(
+          `https://100045.pythonanywhere.com/reports/get-statistics/${portfolio.org_id}/`
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchGraph();
     fetchData(); // Call the async function
   }, [portfolio.org_id]);
 
@@ -56,8 +65,8 @@ function BackupRepo() {
   }
 
   function handleDownload(row) {
-    console.log(row)
-    const fileUrl= row[0].file_url
+    console.log(row);
+    const fileUrl = row[0].file_url;
     const download = async (fileUrl) => {
       const response = await fetch(fileUrl);
       const blob = await response.blob();
@@ -71,127 +80,162 @@ function BackupRepo() {
 
     download();
   }
-      
+
+  const squareli = [];
+  for (var i = 1; i < 365; i++) {
+    const level = Math.floor(Math.random() * 3);
+    squareli.push(<li data-level={level}></li>);
+  }
 
   return (
-    <div className="table-container">
-      <h3>Backup Report</h3>
-      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-        <div className="content">
-          <div className="left">
-            {[
-              "Backup Time ",
-              "Added Files",
-              "Modified Files",
-              "Removed Files",
-              "Created By",
-              "Commit URL",
-            ].map((item) => (
-              <p key={item}>{item}:</p>
-            ))}
+    <>
+      <div className="table-container">
+        <h3>Backup Report</h3>
+        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+          <div className="content">
+            <div className="left">
+              {[
+                "Backup Time ",
+                "Added Files",
+                "Modified Files",
+                "Removed Files",
+                "Created By",
+                "Commit URL",
+              ].map((item) => (
+                <p key={item}>{item}:</p>
+              ))}
+            </div>
+            <div className="right">
+              <p>{selectedData[0]?.backup_time}</p>
+              {selectedData[0]?.added_file &&
+              selectedData[0].added_file.length > 0 ? (
+                // selectedData[0].added_file.map(item => <p>{item}</p>)
+                <p>{selectedData[0].added_file[0]}</p>
+              ) : (
+                <p>No Added Files</p>
+              )}
+              {selectedData[0]?.modified_file &&
+              selectedData[0].modified_file.length > 0 ? (
+                <p>{selectedData[0].modified_file[0]}</p>
+              ) : (
+                <p>No modified Files</p>
+              )}
+              {selectedData[0]?.removed_file &&
+              selectedData[0].removed_file.length > 0 ? (
+                <p>{selectedData[0].removed_file[0]}</p>
+              ) : (
+                <p>No Deleted Files</p>
+              )}
+              <p>{selectedData[0]?.pusher}</p>
+              <p>{selectedData[0]?.commit_url}</p>
+            </div>
           </div>
-          <div className="right">
-            <p>{selectedData[0]?.backup_time}</p>
-            {selectedData[0]?.added_file &&
-            selectedData[0].added_file.length > 0 ? (
-              // selectedData[0].added_file.map(item => <p>{item}</p>)
-              <p>{selectedData[0].added_file[0]}</p>
-            ) : (
-              <p>No Added Files</p>
-            )}
-            {selectedData[0]?.modified_file &&
-            selectedData[0].modified_file.length > 0 ? (
-              <p>{selectedData[0].modified_file[0]}</p>
-            ) : (
-              <p>No modified Files</p>
-            )}
-            {selectedData[0]?.removed_file &&
-            selectedData[0].removed_file.length > 0 ? (
-              <p>{selectedData[0].removed_file[0]}</p>
-            ) : (
-              <p>No Deleted Files</p>
-            )}
-            <p>{selectedData[0]?.pusher}</p>
-            <p>{selectedData[0]?.commit_url}</p>
-          </div>
-        </div>
-      </Popup>
-      {currentItems.length === 0 ? (
-        <Loader />
-      ) : (
-        <>
-          <form>
-            <input
-              className="searchbar"
-              placeholder="search repository Name"
-              onChange={handleChange}
-              name="searchInput"
-              value={searchInput}
-            />
-          </form>
-          <table>
-            <thead>
-              <tr>
-                <th>S.No</th>
-                <th>Repository</th>
-                <th>Zip File</th>
-                <th>Commit Message</th>
-                <th>Show More</th>
-                <th>Download</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data[0] !== "empty" &&
-                currentItems
-                  .filter((item) => {
-                    return searchInput.toLowerCase() === ""
-                      ? item
-                      : item.repository_name
-                          .toLowerCase()
-                          .includes(searchInput);
-                  })
-                  .map((row, rowIndex) => (
-                    <tr className="tabdata" key={rowIndex}>
-                      <td>{startIndex + rowIndex + 1}</td>
-                      <td>{row.repository_name}</td>
-                      <td>{row.zip_file_name}</td>
-                      <td>{row.commit_message}</td>
-                      <td>
-                        <button
-                          className="button-know-more"
-                          onClick={() => handleClick([row])}
-                        >
-                          Show More
-                        </button>
-                      </td>
-                      <td>
+        </Popup>
+        {currentItems.length === 0 ? (
+          <Loader />
+        ) : (
+          <>
+            <form>
+              <input
+                className="searchbar"
+                placeholder="search repository Name"
+                onChange={handleChange}
+                name="searchInput"
+                value={searchInput}
+              />
+            </form>
+            <table>
+              <thead>
+                <tr>
+                  <th>S.No</th>
+                  <th>Repository</th>
+                  <th>Zip File</th>
+                  <th>Commit Message</th>
+                  <th>Show More</th>
+                  <th>Download</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data[0] !== "empty" &&
+                  currentItems
+                    .filter((item) => {
+                      return searchInput.toLowerCase() === ""
+                        ? item
+                        : item.repository_name
+                            .toLowerCase()
+                            .includes(searchInput);
+                    })
+                    .map((row, rowIndex) => (
+                      <tr className="tabdata" key={rowIndex}>
+                        <td>{startIndex + rowIndex + 1}</td>
+                        <td>{row.repository_name}</td>
+                        <td>{row.zip_file_name}</td>
+                        <td>{row.commit_message}</td>
+                        <td>
+                          <button
+                            className="button-know-more"
+                            onClick={() => handleClick([row])}
+                          >
+                            Show More
+                          </button>
+                        </td>
+                        <td>
                           <button
                             className="button-know-more"
                             onClick={() => handleDownload([row])}
                           >
                             Download
                           </button>
-                      </td>
-                    </tr>
-                  ))}
-            </tbody>
-          </table>
-        </>
-      )}
-      <div className="pagination">
-        {Array.from({ length: Math.ceil(data.length / itemsPerPage) }).map(
-          (item, index) => (
-            <button
-              key={index}
-              className={currentPage === index + 1 ? "active" : ""}
-              onClick={() => setCurrentPage(index + 1)}
-            >
-              {index + 1}
-            </button>
-          )
+                        </td>
+                      </tr>
+                    ))}
+              </tbody>
+            </table>
+          </>
         )}
+        <div className="pagination">
+          {Array.from({ length: Math.ceil(data.length / itemsPerPage) }).map(
+            (item, index) => (
+              <button
+                key={index}
+                className={currentPage === index + 1 ? "active" : ""}
+                onClick={() => setCurrentPage(index + 1)}
+              >
+                {index + 1}
+              </button>
+            )
+          )}
+        </div>
       </div>
-    </div>
+      <div className="table-container">
+        <div className="graph">
+          <ul className="months">
+            <li>Jan</li>
+            <li>Feb</li>
+            <li>Mar</li>
+            <li>Apr</li>
+            <li>May</li>
+            <li>Jun</li>
+            <li>Jul</li>
+            <li>Aug</li>
+            <li>Sep</li>
+            <li>Oct</li>
+            <li>Nov</li>
+            <li>Dec</li>
+          </ul>
+          <ul className="days">
+            <li>Sun</li>
+            <li>Mon</li>
+            <li>Tue</li>
+            <li>Wed</li>
+            <li>Thu</li>
+            <li>Fri</li>
+            <li>Sat</li>
+          </ul>
+          <ul className="squares">{ squareli}</ul>
+        </div>
+      </div>
+    </>
   );
 }
 
