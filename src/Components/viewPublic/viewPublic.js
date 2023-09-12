b714f2a6cda05f4903382deefa61094caa4056e7
Halima-Ali <halimaali.ha2002@gmail.com>
2023-09-12 11:58:15 +0300

    public page done

--- src/Components/viewPublic/viewPublic.js
+++ src/Components/viewPublic/viewPublic.js
@@ -10,7 +10,8 @@ export default function ViewPublic() {
   const qr_idParams = queryParams.get("qr_id");
   const company_idParams = queryParams.get("company_id");
   const [data, setData] = React.useState([]);
-  const [qrData, setQrData] = React.useState();
+ const [qrData, setQrData] = React.useState();
+ const [expired, setExpired] = React.useState(false);
 
   React.useEffect(() => {
     // Use useEffect to make the API call and update data state
@@ -25,14 +26,29 @@ export default function ViewPublic() {
         } else {
           console.log(response.data);
           setData(response.data?.qr_ids);
+          let match = response.data?.qr_ids
+            .map((item) => {
+              if (item.qr_ids.includes(qr_idParams)) {
+                return item;
+              }
+            })
+            .filter((item) => item);
+          setQrData(match);
         }
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchMasterLinks();
+   fetchMasterLinks();
+   
+     setTimeout(() => {
+       setExpired(true);
+     }, 6000);
   }, [company_idParams]);
+
+  console.log(qrData);
+
   return (
     <div
       className="container"
@@ -41,37 +57,58 @@ export default function ViewPublic() {
         top: "100px",
         maxWidth: "800px",
         margin: "20px auto",
+        wordWrap: "wrap",
       }}
     >
-      <h2>Share this Product</h2>
-      {/* {data?.qr_ids.map((item) => item)} */}
+      {data.length > 0 ? (
+        <>
+          <h2>Share this Product</h2>
+          <p>
+            <b>Master Link: </b>
+            {qrData[0]?.master_link}
+            <CopyToClipboard
+              text={qrData[0]?.master_link}
+              onCopy={() => alert("Master link copied succesfully")}
+            >
+              <FileCopy className="icon" />
+            </CopyToClipboard>
+          </p>
+          <p>
+            <b>Qr code: </b>
+            {qrData[0]?.qr_code}
 
-      {/* <p>
-        <b>Master Link: </b>
-        <CopyToClipboard
-          text="hrer"
-          onCopy={() => alert("Master link copied succesfully")}
-        >
-          <FileCopy className="icon" />
-        </CopyToClipboard>
-      </p>
-      <p>
-        <b>Qr code: </b>
-        <CopyToClipboard
-          text="qr"
-          onCopy={() => alert("Qr code copied successfully")}
-        >
-          <FileCopy className="icon" />
-        </CopyToClipboard>
-      </p>
-      <center>
-        <img src="{qrCode}" alt="qr-code" />
-      </center> */}
-      <p>{viewParams}</p>
-      <p>{qr_idParams}</p>
-      <p>{company_idParams}</p>
-
-      {data.length > 0 ? <h4>data is not null</h4> : <h4>The data is null</h4>}
+            <CopyToClipboard
+              text={qrData[0]?.qr_code}
+              onCopy={() => alert("Qr code copied successfully")}
+            >
+              <FileCopy className="icon" />
+            </CopyToClipboard>
+          </p>
+          <center>
+            <img src={qrData[0]?.qr_code} alt="qr-code" />
+          </center>
+          <h3>Details about this Link</h3>
+          <p>
+            <b>View Type: </b>
+            {viewParams}
+          </p>
+          <p>
+            <b>Qr_Id: </b> {qr_idParams}
+          </p>
+          <p>
+            <b>Company Id:</b> {company_idParams}
+          </p>
+        </>
+      ) : (
+        <>
+       {!expired && <h4>Loading ...</h4>}
+          {expired && (
+            <p>
+              The link is expired. please contact owner for correct Link details
+            </p>
+          )}
+        </>
+      )}
     </div>
   );
 }
